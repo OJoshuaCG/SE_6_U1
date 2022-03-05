56a8f51472243c65db49991f0a7b67bb922bee1f
Leo-Zubiri <leo.zubiri@outlook.com>
2022-03-05 13:59:46 -0600

    dist2puntos

--- ino_ejercicios/Prog_04_Distancia2Puntos/Prog_04_Distancia2Puntos.ino
+++ ino_ejercicios/Prog_04_Distancia2Puntos/Prog_04_Distancia2Puntos.ino
@@ -2,25 +2,94 @@
   Distancia entre dos puntos, dadas las coordenadas
 */
 
-enum Edos{setX1,setY1,setX2,setY2,repeat,finalizado};
+enum Edos{
+  getX1,getY1,getX2,getY2,
+  setX1,setY1,setX2,setY2,
+  calcular,repetir,finalizado
+  };
 
-Edos estado = setX1;
+int estado = getX1;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
-float x1,x2,y1,y2,dist,v;
-float value;
+float x1,x2,y1,y2,dist;
+int value;
+String temp;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available()>0){
-    value = Serial.readString().toFloat();
-    switch(estado){
-      case setX1: x1 = value; estado = setX2; break;
+  switch(estado){
+    case getX1:
+      Serial.println("Distancia entre dos puntos");
+      Serial.print("X1: ");
+      estado = setX1;
+      break;
+    case setX1:
+      if (Serial.available() > 0) {
+        temp = Serial.readString();
+        Serial.print(temp + "\n");
+        x1 = temp.toFloat();
+        estado = getY1;
+      }
+      break;
+    case getY1:
+      Serial.print("Y1: ");
+      estado = setY1;
+      break;
+    case setY1:
+      if (Serial.available() > 0) {
+        temp = Serial.readString();
+        Serial.print(temp + "\n");
+        y1 = temp.toFloat();
+        estado = getX2;
+      }
+      break;
       
-    }
+    case getX2:
+      Serial.print("X2: ");
+      estado = setX2;
+      break;
+    case setX2:
+      if (Serial.available() > 0) {
+        temp = Serial.readString();
+        Serial.print(temp + "\n");
+        x2 = temp.toFloat();
+        estado = getY2;
+      }
+      break;
+    case getY2:
+      Serial.print("Y2: ");
+      estado = setY2;
+      break;
+    case setY2:
+      if (Serial.available() > 0) {
+        temp = Serial.readString();
+        Serial.print(temp + "\n");
+        y2 = temp.toFloat();
+        estado = calcular;
+      }
+      break;
+    case calcular:
+      dist = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+      Serial.println("\n La distancia es: " + String(dist));
+      Serial.println("Quieres calcular otra distancia? \n 1.Si 2.No");
+      estado = repetir;
+      break;
+    case repetir:
+      if(Serial.available()>0){
+        temp= Serial.readString();
+        value = temp.toInt();
+        if(value == 1) estado = getX1;
+        else{
+           Serial.println("Adios !!!");
+           estado = finalizado;
+        }
+      } 
+      break;
+    case finalizado:
+      break;  
   }
 }
